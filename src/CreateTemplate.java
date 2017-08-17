e2c0c2ffc1ef028091c94002438bcdc2f6729050
wangchenghao <1021717079@qq.com>
2017-08-17 19:11:24 +0800

    设置标题标题

--- src/CreateTemplate.java
+++ src/CreateTemplate.java
@@ -1,10 +1,12 @@
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.usermodel.*;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.jdom.Attribute;
 import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.input.SAXBuilder;
 
 import java.io.File;
+import java.util.List;
 
 /**
  * Created by wangchenghao on 2017/8/16.
@@ -37,7 +39,43 @@ public class CreateTemplate {
             int column = 0;
             //设置列宽
             Element colgroup = root.getChild("colgroup");
+            setColumnWidth(sheet, colgroup);
 
+            //设置标题 合并单元格
+            Element title = root.getChild("title");
+            List<Element> trs = title.getChildren("tr");
+            for(int i=0; i<trs.size(); i++){
+                Element tr = trs.get(i);
+                List<Element> tds = tr.getChildren("rd");
+                HSSFRow row = sheet.createRow(rownum);
+                HSSFCellStyle cellStyle = wb.createCellStyle();
+                cellStyle.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+                for(column=0; column<tds.size(); column++){
+                    Element td = tds.get(column);
+                    HSSFCell cell = row.createCell(column);
+                    Attribute rowSpan = td.getAttribute("rowspan");
+                    Attribute colSpan = td.getAttribute("colspan");
+                    Attribute value = td.getAttribute("value");
+                    if (value != null){
+                        String val = value.getValue();
+                        cell.setCellValue(val);
+                        int rspan = rowSpan.getIntValue() - 1;
+                        int cspan = colSpan.getIntValue() - 1;
+
+                        //设置字体
+                        HSSFFont font = wb.createFont();
+                        font.setFontName("仿宋_GB2312");
+                        font.setBold(true);
+                        font.setFontHeight((short)12);
+                        cellStyle.setFont(font);
+                        cell.setCellStyle(cellStyle);
+                        //合并单元格并居中
+                        sheet.addMergedRegion(new CellRangeAddress(rspan, rspan, 0, cspan));
+                    }
+                }
+                rownum++;
+            }
+            
 
         }catch (Exception e){
             e.printStackTrace();
@@ -50,6 +88,21 @@ public class CreateTemplate {
      * @param colgroup
      */
     private static void setColumnWidth(HSSFSheet sheet, Element colgroup){
+        List<Element> cols = colgroup.getChildren("col");
+        for(int i=0; i<cols.size(); i++){
+            Element col = cols.get(i);
+            Attribute width = col.getAttribute("width");
+            String unit = width.getValue().replaceAll("[0-9,\\.]","");//判断列宽的单位
+            String value = width.getValue().replaceAll(unit, "");//获得数字
+            int v = 0;
+            if(unit == null || "px".endsWith(unit)){
+                v = Math.round(Float.parseFloat(value) * 37F);
+            }else if("em".endsWith(unit)){
+                v = Math.round(Float.parseFloat(value) * 267.5F);
+            }
+
+            sheet.setColumnWidth(i, v);
+        }
 
     }
 }
